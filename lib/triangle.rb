a3d87865dc7e56fb000281f30ceb0b3b6912c4ba
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-04 01:51:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,12 +11,12 @@ class Triangle
   end
 
   def kind
-    if side_1 + side_2 < side_3 || side_1 + side_3 < side_2 || side_2 + side_3 < side_1 || side_1 == 0 || side_2 == 0 || side_3 == 0
+    if @side_1 + @side_2 < @side_3 || @side_1 + @side_3 < @side_2 || @side_2 + @side_3 < @side_1 || @side_1 == 0 || @side_2 == 0 || @side_3 == 0
       begin
         raise TriangleError
       rescue TriangleError => error
         puts error.message
-      end 
+      end
     else
       if side_1 != side_2 && side_2 != side_3 && side_3 != side_1
         @type = :scalene
