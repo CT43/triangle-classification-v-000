9f09f373b98729ae091bab62d7d6a483e6d9a95e
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-04 01:38:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,21 +11,21 @@ class Triangle
   end
 
   def kind
-    if side_1 + side_2 < side_3 || side_1 + side_3 < side_2 || side_2 + side_3 < side_1 || side_1 =< 0 || side_2 =< 0 || side_3 =< 0 
-      begin 
-        raise TriangleError 
-      rescue TriangleError => error 
-        puts error.message 
-      end 
+    if side_1 + side_2 < side_3 || side_1 + side_3 < side_2 || side_2 + side_3 < side_1 || side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
     else
-      if side_1 != side_2 != side_3 
+      if side_1 != side_2 != side_3
         @type = scalene
-      elsif side_1 = side_2 = side_3 
-        @type = equilateral 
-      else 
+      elsif side_1 = side_2 = side_3
+        @type = equilateral
+      else
         @type = isosceles
-      end  
-    end 
+      end
+    end
   end
 
   def self.all
