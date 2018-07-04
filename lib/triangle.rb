eb9dcb0942164793d21715fea433fbc090f058c6
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-04 01:59:25 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
 
   def kind
     if side_1 + side_2 < side_3 || side_1 + side_3 < side_2 || side_2 + side_3 < side_1 || side_1 == 0 || side_2 == 0 || side_3 == 0
-      begin
+      return begin
         raise TriangleError
       rescue TriangleError => error
         puts error.message
@@ -27,8 +27,6 @@ class Triangle
       end
     end
   end
-
-
 
   class TriangleError < StandardError
     def message
