52c6427967f55e68fbeba912bee265d871697913
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-04 01:35:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,17 +10,31 @@ class Triangle
     self.class.all << self
   end
 
-  def kind 
-    
-  end 
+  def kind
+    if side_1 + side_2 < side_3 || side_1 + side_3 < side_2 || side_2 + side_3 < side_1 || side_1 =< 0 || side_2 =< 0 || side_3 =< 0 
+      begin 
+        raise TriangleError 
+      rescue TriangleError => error 
+        puts error.message 
+      end 
+    else
+      if side_1 != side_2 != side_3 
+        @type = scalene
+      elsif side_1 = side_2 = side_3 
+        @type = equilateral 
+      else 
+        @type = isosceles
+      end  
+    end 
+  end
 
   def self.all
     @@all
-  end 
+  end
 
-  class TriangleError < StandardError 
-    def message 
+  class TriangleError < StandardError
+    def message
       'This "triangle" is not a triangle!!! Check the side lengths and try again'
-  end 
+  end
 
 end
